a9256beb9b6f5594ba6e9c7294bbd4be7e83851f
lzcm729 <lzcm.729@gmail.com>
2025-06-28 20:00:31 +0800

    衣柜关门

--- player/Monster/closet.gd
+++ player/Monster/closet.gd
@@ -1,26 +1,15 @@
 extends Player
 
 const MAX_ANGLE := -45.0
-const ROT_SPEED := 30.0  # 度/秒
+const ROT_SPEED := 25.0  # 度/秒
 
-var start_remove := false
-var removing := false
+var opened := false
 
 @onready var toy = $Path3D/PathFollow3D/Doll
 @onready var closet:    MeshInstance3D = $"Mesh/柜子"
-@onready var door_pivot: Node3D       = $Mesh/Left
+@onready var left_door: Node3D       = $Mesh/Left
 @onready var door_sound: AudioStreamPlayer3D = $Sound
 
-
-# 缓慢打开门到指定角度（度），用时 duration 秒
-func open_door_slow(angle_deg: float, duration: float) -> void:
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_SINE)
-	tween.set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(door_pivot, "rotation_degrees:y", angle_deg, duration)
-	tween.tween_callback(remove_toy)
-	door_sound.play()	
-	
 
 func _ready() -> void:
 	pass
@@ -39,7 +28,39 @@ func _ready() -> void:
 # 		toy.queue_free()
 
 
+func _process(delta):
+	if Input.is_action_pressed("interact"):  # 默认是空格，或自己在项目设置里映射一个动作
+		if opened:
+			return  # 如果已经打开了，就不再处理
+		# 启动音效（按下时只播一次）
+		if door_sound.playing == false:
+			door_sound.play()
+		# 持续往目标角度靠
+		var y = left_door.rotation_degrees.y
+		y = max(y - ROT_SPEED * delta, MAX_ANGLE)
+		left_door.rotation_degrees.y = y
+		# 到位后开始移除玩具
+		if y <= MAX_ANGLE:
+			remove_toy()
+			opened = true
+	else:
+		# 松开键后只要不再按就不会再转
+		door_sound.stop()
+
+
+# 缓慢打开门到指定角度（度），用时 duration 秒
+func open_door_slow(angle_deg: float, duration: float) -> void:
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(left_door, "rotation_degrees:y", angle_deg, duration)
+	tween.tween_callback(remove_toy)
+	door_sound.play()	
+	
+
 func remove_toy() -> void:
+	if not toy:
+		return  # 如果玩具已经被移除，则不再执行
 	var mover = toy.get_parent() as PathFollow3D
 	var close_speed = 3.0
 	var duration = mover.progress / close_speed    # 距离／速度 = 时间
@@ -48,22 +69,15 @@ func remove_toy() -> void:
 	tween.tween_property(mover, "progress", 0.0, duration)
 	tween.tween_callback(func():
 		toy.queue_free()
+		shut_door()
 	)
 
 
-func _process(delta):
-	if Input.is_action_pressed("interact"):  # 默认是空格，或自己在项目设置里映射一个动作
-		# 启动音效（按下时只播一次）
-		if door_sound.playing == false:
-			door_sound.play()
-		# 持续往目标角度靠
-		var y = door_pivot.rotation_degrees.y
-		y = max(y - ROT_SPEED * delta, MAX_ANGLE)
-		door_pivot.rotation_degrees.y = y
-		# 到位后开始移除玩具
-		if y <= MAX_ANGLE and not removing:
-			removing = true
-			remove_toy()
-	else:
-		# 松开键后只要不再按就不会再转
-		door_sound.stop()
+# 关闭门
+func shut_door() -> void:
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(left_door, "rotation_degrees:y", 0.0, 0.1)
+	tween.tween_callback(remove_toy)
+	door_sound.play()
