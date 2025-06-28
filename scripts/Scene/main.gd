e1d704420ec241d12b2ee99694e3775ec075d8f1
q693521426 <693521426@qq.com>
2025-06-28 20:12:38 +0800

    fix

--- scripts/Scene/main.gd
+++ scripts/Scene/main.gd
@@ -2,7 +2,7 @@ extends Node3D
 class_name MainScene
 @onready var PostEffect_CRT: CanvasLayer = $CanvasLayer_CRT
 
-var currentLevelIndex = 1
+var currentLevelIndex = 2
 var levels = [] as Array[Level]
 func switch_level(index:int) ->void:
 	# 添加新场景到场景树
@@ -11,13 +11,15 @@ func switch_level(index:int) ->void:
 			add_child(level)
 			level.scene = self
 		else:
-			remove_child(level)
+			if self.is_ancestor_of(level):
+				remove_child(level)
 
 
 func _ready() -> void:
 	for level in get_tree().get_nodes_in_group("LevelGroup"):
-		remove_child(level)
-		levels.append(level)
+		if self.is_ancestor_of(level):
+			remove_child(level)
+			levels.append(level)
 	PostEffect_CRT.hide()		
 	switch_level(currentLevelIndex)
 	
