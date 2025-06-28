3b0e41f7d1d6c4d5e3b8176b2177d03fb2d5e4bb
lzcm729 <lzcm.729@gmail.com>
2025-06-29 01:43:34 +0800

    draw

--- player/Obstacle/light.gd
+++ player/Obstacle/light.gd
@@ -1,4 +1,5 @@
 extends Node3D
+class_name Lamp
 
 var default_radius: float
 var min_radius: float = 0.2
@@ -17,19 +18,20 @@ func _ready() -> void:
 	else:
 		push_error("Decal mesh is not a CylinderMesh!")
 
-func _process(delta: float) -> void:
-	if gone: return
 
-	if Input.is_action_pressed("interact"):
-		if cylinder_mesh.bottom_radius > min_radius:
-			cylinder_mesh.bottom_radius = cylinder_mesh.bottom_radius - shrink_rate * delta
-		else:
-			print("Light is gone")
-			decal.visible = false
-			light.visible = false
-			decal.queue_free()
-			light.queue_free()
-			gone = true
+func decrease_light(delta) -> void:
+	if gone: return
+	if cylinder_mesh.bottom_radius > min_radius:
+		cylinder_mesh.bottom_radius = cylinder_mesh.bottom_radius - shrink_rate * delta
 	else:
-		if cylinder_mesh.bottom_radius < default_radius:
-			cylinder_mesh.bottom_radius = cylinder_mesh.bottom_radius + shrink_rate * delta
+		print("Light is gone")
+		decal.visible = false
+		light.visible = false
+		decal.queue_free()
+		light.queue_free()
+		gone = true
+	
+	
+func increase_light(delta) -> void:
+	if cylinder_mesh.bottom_radius < default_radius:
+		cylinder_mesh.bottom_radius = cylinder_mesh.bottom_radius + shrink_rate * delta
