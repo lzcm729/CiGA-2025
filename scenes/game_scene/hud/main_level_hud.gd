c0d6062efcbc714a5415c0868ef6ea1ef7457e81
ByronZyh <43203171+ByronZyh@users.noreply.github.com>
2025-06-28 19:26:33 +0800

    开始倒计时

--- scenes/game_scene/hud/main_level_hud.gd
+++ scenes/game_scene/hud/main_level_hud.gd
@@ -1,1 +1,18 @@
 extends Control
+
+@export var gameplay:GamePlay
+@onready var count_down_num: Label = $CountDownMarginContainer/Control/Num
+@onready var goal_panel: Panel = $GoalMarginContainer/Control/Panel
+
+
+func _ready() -> void:
+	gameplay.TIME_COUNTDOWN.connect(on_time_countdown)
+
+func on_time_countdown(cur_times:int):
+	count_down_num.change_num(cur_times)
+
+func show_guide_panel(is_show:bool):
+	goal_panel.visible = is_show
+
+	
+	
