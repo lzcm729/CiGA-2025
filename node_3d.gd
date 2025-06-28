20fca865389aec8b2b78b0e35cac19af855d53ea
lzcm729 <lzcm.729@gmail.com>
2025-06-28 13:33:22 +0800

    开门音效

--- node_3d.gd
+++ node_3d.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @onready var closet:    MeshInstance3D = $Closet
 @onready var door_pivot: Node3D       = $Node3D
+@onready var door_sound: AudioStreamPlayer3D = $DoorSound
+
 
 # 缓慢打开门到指定角度（度），用时 duration 秒
 func open_door_slow(angle_deg: float, duration: float) -> void:
@@ -9,6 +11,8 @@ func open_door_slow(angle_deg: float, duration: float) -> void:
 		.set_trans(Tween.TRANS_SINE)\
 		.set_ease(Tween.EASE_IN_OUT)\
 		.tween_property(door_pivot, "rotation_degrees:y", angle_deg, duration)
+	door_sound.play()	
+	
 
 func _ready() -> void:
 	# 1.5 秒内打开 45°
