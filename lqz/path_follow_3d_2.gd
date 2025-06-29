ab8ef702e88cde3719cc395827bec1cf3c9e069d
lzcm729 <lzcm.729@gmail.com>
2025-06-29 13:57:46 +0800

    路径按空格显示

--- lqz/path_follow_3d_2.gd
+++ lqz/path_follow_3d_2.gd
@@ -6,8 +6,18 @@ var length : float                   # 路径总长（像素）
   
 func _ready():
 	length = get_parent().curve.get_baked_length()
+	set_process(false)      # start disabled
 
 func _process(delta):
-	progress_ratio += speed * delta / length   # progress ∈ [0,1]
-	if progress_ratio > 1.0:
-		queue_free()  # 跑完自动删除；或 progress = 0 循环
+	progress_ratio += speed * delta / length
+	if progress_ratio >= 1.0:
+		progress_ratio = 0
+		set_process(false)
+
+func ShowOnce():
+	progress_ratio = 0
+	set_process(true)
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("jump"):
+		ShowOnce()
