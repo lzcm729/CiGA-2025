31b4a6b0f45245e69a38870b8e9814b75c7dc503
lzcm729 <lzcm.729@gmail.com>
2025-06-29 13:31:47 +0800

    draw

--- player/Monster/draw.gd
+++ player/Monster/draw.gd
@@ -3,6 +3,11 @@ class_name Draw
 
 
 @export var lamp : Lamp
+@export var move_speed: float = 1.0             # 移动速度
+@export var min_y: float = -3.0                 # 最低允许下降的 y 坐标
+@onready var mesh_node: Node3D = $"Mesh"       # 引用 Mesh 节点
+@onready var init_y: float = $"Mesh".position.y  # 记录原始高度
+
 var is_finished := false
 
 
@@ -10,8 +15,16 @@ func _process(delta: float) -> void:
 	if not is_current: return
 	if is_finished: return
 	if lamp.is_relighting: return
+
 	if Input.is_action_pressed("interact"):
 		var res = lamp.decrease_light(delta)
+		# ↓ 限制 Mesh 最低高度
+		var dy = move_speed * delta
+		var curr_y = mesh_node.position.y
+		if curr_y - dy > min_y:
+			mesh_node.translate(Vector3(0, -dy, 0))
+		else:
+			mesh_node.position.y = min_y
 		if res:
 			start_action.emit(Consts.ITEMS.DRAW)
 		else:
@@ -19,6 +32,13 @@ func _process(delta: float) -> void:
 			is_finished = true
 	else:
 		lamp.increase_light(delta)
+		# ↑ 未按下时上升，但不超原位
+		var rise = move_speed * delta
+		var curr_y = mesh_node.translation.y
+		if curr_y + rise < init_y:
+			mesh_node.translate(Vector3(0, rise, 0))
+		else:
+			mesh_node.translation.y = init_y
 		stop_action.emit(Consts.ITEMS.DRAW)
 
 
