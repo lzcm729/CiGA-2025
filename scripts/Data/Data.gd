fdd0db5aa29df821425e813256716847a4fbdfc5
Yilun Wang <46103093+Leon211@users.noreply.github.com>
2025-06-29 15:32:22 +0800

    draw 关卡时长

--- scripts/Data/Data.gd
+++ scripts/Data/Data.gd
@@ -4,10 +4,10 @@ extends Node
 const LEVEL_MAX = 4
 const LEVEL_INFO = {
 	# 关卡对应的数据设置
-	1 : { "node_name" : "level1", "level_path":"res://scenes/level/level1.tscn", "total_time" : 300.0, "item_list" : [Consts.ITEMS.BOOK], "BLOCKS" : [Consts.BLOCKS.BEAR]},
-	2 : { "node_name" : "level2", "level_path":"res://scenes/level/level2.tscn", "total_time" : 300.0, "item_list" : [], "BLOCKS" : []},
-	3 : { "node_name" : "level3", "level_path":"res://scenes/level/level3.tscn", "total_time" : 300.0, "item_list" : [], "BLOCKS" : []},
-	4 : { "node_name" : "level4", "level_path":"res://scenes/level/level4.tscn", "total_time" : 300.0, "item_list" : [], "BLOCKS" : []},
+	1 : { "node_name" : "level1", "level_path":"res://scenes/level/level1.tscn", "total_time" : 60.0, "item_list" : [Consts.ITEMS.BOOK], "BLOCKS" : [Consts.BLOCKS.BEAR]},
+	2 : { "node_name" : "level2", "level_path":"res://scenes/level/level2.tscn", "total_time" : 120.0, "item_list" : [], "BLOCKS" : []},
+	3 : { "node_name" : "level3", "level_path":"res://scenes/level/level3.tscn", "total_time" : 160.0, "item_list" : [], "BLOCKS" : []},
+	4 : { "node_name" : "level4", "level_path":"res://scenes/level/level4.tscn", "total_time" : 200.0, "item_list" : [], "BLOCKS" : []},
 }
 
 # 场景数据
